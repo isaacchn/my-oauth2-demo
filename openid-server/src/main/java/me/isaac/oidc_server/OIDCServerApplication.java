c3c0f5a9ec244728180f7d5c4aeaa9bafbe7423b
Isaac <magicyangsd@163.com>
2022-07-28 02:13:18 +0800

    重新创建

---
+++ openid-server/src/main/java/me/isaac/oidc_server/OIDCServerApplication.java
@@ -0,0 +1,11 @@
+package me.isaac.oidc_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OIDCServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OIDCServerApplication.class, args);
+    }
+}
