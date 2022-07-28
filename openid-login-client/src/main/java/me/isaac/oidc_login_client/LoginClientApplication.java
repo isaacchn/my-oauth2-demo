a33f7d6e2249bc94fd7d613801ceb366828221ff
Isaac <magicyangsd@gmail.com>
2022-07-28 14:57:52 +0800

    login-client能够通过auth-server实现登录，但暂时无法调用/userinfo端点。

---
+++ openid-login-client/src/main/java/me/isaac/oidc_login_client/LoginClientApplication.java
@@ -0,0 +1,11 @@
+package me.isaac.oidc_login_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoginClientApplication.class, args);
+    }
+}
--- openid-server/src/main/java/me/isaac/oidc_server/config/AuthServerConfig.java
+++ openid-server/src/main/java/me/isaac/oidc_server/config/AuthServerConfig.java
@@ -5,19 +5,24 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
+import org.springframework.context.annotation.Role;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -36,13 +41,17 @@ import java.util.UUID;
 @Configuration
 public class AuthServerConfig {
     @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Order(1)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
         //未认证时重定向到登录界面
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
-        httpSecurity.exceptionHandling(exception -> exception
-                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
-        );
+        httpSecurity
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+        //return httpSecurity.formLogin(Customizer.withDefaults()).build();
         return httpSecurity.build();
     }
 
@@ -52,7 +61,8 @@ public class AuthServerConfig {
         //重定向到登录界面
         httpSecurity.authorizeHttpRequests(authorizeRequest -> authorizeRequest
                 .anyRequest().authenticated()
-        ).formLogin(Customizer.withDefaults());
+        )
+                .formLogin(Customizer.withDefaults());
         return httpSecurity.build();
     }
 
@@ -69,17 +79,16 @@ public class AuthServerConfig {
     @Bean
     public RegisteredClientRepository registeredClientRepository() {
         RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("messaging-client")
-                .clientSecret("s3cret")
+                .clientId("login-client")
+                .clientSecret("{noop}secret") //todo 这里改成错的要如何拦截到？
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
-                .redirectUri("http://127.0.0.1:8080/authorized")
+                .redirectUri("http://127.0.0.1:18070/login/oauth2/code/login-client")
+                .redirectUri("http://127.0.0.1:18070/authorized")
                 .scope(OidcScopes.OPENID)
-                .scope("message.read")
-                .scope("message.write")
+                .scope(OidcScopes.PROFILE)
                 .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
                 .build();
         return new InMemoryRegisteredClientRepository(registeredClient);
@@ -96,6 +105,25 @@ public class AuthServerConfig {
 //    }
 
     @Bean
+    public JwtDecoder jwtDecoder(KeyPair keyPair) {
+        return NimbusJwtDecoder.withPublicKey((RSAPublicKey) keyPair.getPublic()).build();
+    }
+
+    @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+        return keyPair;
+    }
+
+    @Bean
     public JWKSource<SecurityContext> jwkSource() {
         //An instance of com.nimbusds.jose.jwk.source.JWKSource for signing access tokens.
         KeyPair keyPair = generateRsaKey();
@@ -110,18 +138,18 @@ public class AuthServerConfig {
     }
 
 
-    private static KeyPair generateRsaKey() {
-        //	An instance of java.security.KeyPair with keys generated on startup used to create the JWKSource above
-        KeyPair keyPair;
-        try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-            keyPairGenerator.initialize(2048);
-            keyPair = keyPairGenerator.generateKeyPair();
-        } catch (Exception ex) {
-            throw new IllegalStateException(ex);
-        }
-        return keyPair;
-    }
+//    private static KeyPair generateRsaKey() {
+//        //	An instance of java.security.KeyPair with keys generated on startup used to create the JWKSource above
+//        KeyPair keyPair;
+//        try {
+//            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+//            keyPairGenerator.initialize(2048);
+//            keyPair = keyPairGenerator.generateKeyPair();
+//        } catch (Exception ex) {
+//            throw new IllegalStateException(ex);
+//        }
+//        return keyPair;
+//    }
 
     @Bean
     public ProviderSettings providerSettings() {
